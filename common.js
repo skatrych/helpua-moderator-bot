ccd2e149226c29ca980a1c4dbf41982e0987e524
Sergii Katrych <sergii.katrych.ext@siemens-energy.com>
2023-03-20 21:42:03 +0100

    feat: move deleteBadWords and handleRegularMessage into common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@
 module.exports = {
 
     isWhitelistedChat: (chatId) => {
-        if (chatId == process.env.CHAT_ID) { // only my test chat
+        if (chatId === process.env.CHAT_ID) { // only my test chat
             return true;
         }
         console.log('Message in NOT whitelisted chat', chatId);
@@ -20,5 +20,27 @@ module.exports = {
 
         // Check if this is the first message sent by this user in the chat
         return (chatMember.status === 'left');
-    }
+    },
+
+    deleteBadWords: async (msg, bot) => {
+        const chatId = msg.chat.id;
+        const message = msg.text;
+
+        // Define the list of inappropriate words
+        const badWords = ['tesst', 'bad_word_1', 'bad_word_2'];
+
+        // Check if the message contains any inappropriate language
+        if (message && badWords.some(badWord => message.includes(badWord))) {
+            await bot.deleteMessage(chatId, msg.message_id)
+                .catch((e) => {
+                    console.log("Failed deleting message", message, e);
+                });
+        }
+    },
+
+    handleRegularMessage: (msg) => {
+        // nothing to do for now
+        return false;
+    },
+
 }
