dc8d418975b24e08c05f79deb6db0aa60351c87c
Sergii Katrych <sergii.katrych.ext@siemens-energy.com>
2023-03-20 21:44:02 +0100

    feat: switch to telegraf lib

--- main.js
+++ main.js
@@ -4,83 +4,63 @@ require('dotenv').config();
 // import * as Common from './common.js';
 // import * as TelegramBot from 'node-telegram-bot-api';
 
-const TelegramBot = require('node-telegram-bot-api');
+const { express } = require('express');
+// import express from "express";
+const { Telegraf } = require('telegraf');
+// import { Telegraf } from "telegraf";
+const { message } = require('telegraf/filters');
+// import { message } from "telegraf/filters";
+
 const NewChatMembers = require('./new-chat-members.js');
 const Common = require('./common.js');
 const ReadOnlyTimeHandler = require('./handle-readonly-time.js');
 
 // Create the bot instance
-const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+// const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const app = express();
 const newChatMembers = new NewChatMembers(bot);
 const readOnlyTimeHandler = new ReadOnlyTimeHandler(bot);
 
-bot.on('message', async (msg) => {
+// Set the bot API endpoint
+app.use(await bot.createWebhook({ domain: webhookDomain }));
+
+bot.on(message('new_chat_members'), async (ctx) => {
+    const joinOrLeaveHandled = await newChatMembers.handleJoinOrLeaveSystemMessage(ctx.message);
+    // if (joinOrLeaveHandled) return; // handled system message => no further actions required
+});
+
+bot.on(message('left_chat_member'), async (ctx) => {
+    const joinOrLeaveHandled = await newChatMembers.handleJoinOrLeaveSystemMessage(ctx.message);
+    // if (joinOrLeaveHandled) return; // handled system message => no further actions required
+});
+
+bot.on(message('text'), async (ctx) => {
+    ctx.reply('👍');
+    console.log(ctx.message);
+    console.log(ctx);
     // skip operation if the message is not from whitelisted chat
-    // if (!Common.isWhitelistedChat(msg.chat.id)) return;
+    // if (!Common.isWhitelistedChat(ctx.message.chat.id)) return;
 
     // console.log('Message obj:', msg);
 
-    const joinOrLeaveHandled = await newChatMembers.handleJoinOrLeaveSystemMessage(msg);
-    if (joinOrLeaveHandled) return; // handled system message => no further actions required
-
-    const handledAsSilentTime = await readOnlyTimeHandler.handleSilentTime(msg);
+    const handledAsSilentTime = await readOnlyTimeHandler.handleSilentTime(ctx.message);
     if (handledAsSilentTime) return; // message silenced => no further actions required
 
-    await deleteBadWords(msg, bot);
+    await Common.deleteBadWords(ctx.message, bot);
 
     // Just FYI:
     // const actualMessage = msg.text;
     // const userId = msg.from.id;
     // console.log('Message obj:', msg);
 
-    const chatMemberStatus = await bot.getChatMember(msg.chat.id, msg.from.id);
-    const handledAsFirstMessageOfRecentMember = await newChatMembers.handleFirstMessageOfNewMember(msg, chatMemberStatus);
+    const chatMemberStatus = await bot.getChatMember(ctx.message.chat.id, ctx.message.from.id);
+    const handledAsFirstMessageOfRecentMember = await newChatMembers.handleFirstMessageOfNewMember(ctx.message, chatMemberStatus);
 
     !handledAsFirstMessageOfRecentMember &&
-        handleRegularMessage(msg);
+    Common.handleRegularMessage(ctx.message);
 
 });
 
-let deleteBadWords = async (msg, bot) => {
-    const chatId = msg.chat.id;
-    const message = msg.text;
-
-    // Define the list of inappropriate words
-    const badWords = ['tesst', 'bad_word_1', 'bad_word_2'];
-
-    // Check if the message contains any inappropriate language
-    if (message && badWords.some(badWord => message.includes(badWord))) {
-        await bot.deleteMessage(chatId, msg.message_id)
-            .catch((e) => {
-                console.log("Failed deleting message", message, e);
-            });
-    }
-}
-
-function handleRegularMessage(msg) {
-    // nothing to do for now
-    return false;
-}
-
-/*
-let handleUserTime = async (msg, bot) => {
-    // Get the user who sent the message
-    // const user = ctx.message.from;
-    const user = msg.from.id;
-
-    // Check if this is the first message from this user in the group
-    if (!userJoinTimes.has(user.id)) {
-        // Store the current time as the time the user joined the group
-        userJoinTimes.set(user.id, new Date());
-
-        // Send a direct message to the user with a welcome message
-        ctx.telegram.sendMessage(user.id, `Welcome to the group! You have been a member for 0 seconds.`);
-    } else {
-        // Calculate the difference between the current time and the time the user joined the group
-        const joinTime = userJoinTimes.get(user.id);
-        const timeDiff = (new Date() - joinTime) / 1000;
-
-        // Send a direct message to the user with the time they have been a member of the group
-        ctx.telegram.sendMessage(user.id, `You have been a member of the group for ${timeDiff} seconds.`);
-    }
-}*/
+bot.launch();
+app.listen(port, () => console.log("Listening on port", process.env.BOT_HTTP_PORT));
